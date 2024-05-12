a6f1cbdfdea26a4bfc0c2b2235200197230fe48d
sanjaymoglix <sanjay.kumar@moglix.com>
2024-05-12 14:25:36 +0530

    --

--- JavaScript-Tutorial/demo.js
+++
@@ -1,9 +0,0 @@
-var employee1 = { firstName: "John", lastName: "Rodson" };
-var employee2 = { firstName: "Jimmy", lastName: "Baily" };
-
-function invite(greeting1, greeting2) {
-    console.log(greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2);
-}
-
-invite.call(employee1, "Hello", "How are you?"); // Hello John Rodson, How are you?
-invite.call(employee2, "Hello", "How are you?"); // Hello Jimmy Baily, How are you?
